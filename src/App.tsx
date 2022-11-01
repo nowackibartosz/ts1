8b4523180dd66d47a2bf5ab867007d935e71f335
Bartosz Nowacki <nowackib@protonmail.com>
2022-11-01 22:54:35 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,8 @@
 import "./App.css";
 
 function App() {
-  let a = "tesdt";
-  type FirstObj = { name: string; surname2: string };
-
-  const arr: Array<string> = ["adas"];
-  const arr2: FirstObj[] = [
-    {
-      name: "Adam",
-      surname2: "Jochemczyk",
-    },
-  ];
-
-  // const obj: FirstObj = {
-  //   name: "Adam",
-  //   surname2: "Jochemczyk",
-  // };
-
-  // function setMargin(value: string | number) {
-  //   return value;
-  // }
-
-  // type Cases = "add" | "subtract";
-
-  // function someSw(cases: Cases) {
-  //   switch (cases) {
-  //     case "add":
-  //       break;
-  //     case "subtract":
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
-
-  // interface Kid {
-  //   name: string;
-  //   parentName?: string;
-  //   age: number;
-  //   introduce: () => void;
-  // }
-
-  // type Type2 = {
-  //   readonly name: string;
-  //   age: number;
-  //   kids: Kid[];
-  // };
-
-  // const human: Type2 = {
-  //   name: "s",
-  //   age: 55,
-  //   kids: [
-  //     {
-  //       name: "Jolanta",
-  //       parentName: "Max",
-  //       age: 233,
-  //       introduce: () => {
-  //         console.log("siemanko");
-  //       },
-  //     },
-  //   ],
-  // };
-  // type Siema = {
-  //   witam: string;
-  // };
-
-  // //generyki
-  // function genericEx<T>(arg: T): T {
-  //   return arg;
-  // }
-
-  // genericEx<Kid>();
+  
+ 
 
   return (
     <div className="App">
@@ -79,7 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
 
-        <p>{a}</p>
+        <p></p>
       </header>
     </div>
   );
